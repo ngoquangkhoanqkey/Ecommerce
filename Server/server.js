180aa4c164b6f2be188bf8f020fa676013015656
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2024-03-02 14:52:24 +0700

    #1 code base

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000
+app.use(express.json())
+app.use(express.urlencoded({extends:true}))
+
+app.use('/', (req,res)=>{res.send('Server running')})
+
+app.listen(port, ()=> {
+    console.log('Server running on the port',+ port);
+})
