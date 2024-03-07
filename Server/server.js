1f8594826c0f2ce10e17ab0aea3143ca6da69a4f
ngoquangkhoanqkey <ngoquangkhoanqk@gmail.com>
2024-03-07 17:28:58 +0700

    connect db and create document

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,15 @@
 const express = require("express")
 require('dotenv').config()
+const dbConnect = require('./config/dbconnect')
+const initRoutes = require('./router')
 
 const app = express()
 const port = process.env.PORT || 5000
 app.use(express.json())
+// Urlencode giup t doc dc data gui theo kieu Array, Object: Ta lay dc, doc duoc. Extends giup doc dc nhieu hon
 app.use(express.urlencoded({extends:true}))
-
-app.use('/', (req,res)=>{res.send('Server running')})
+dbConnect()
+initRoutes(app)
 
 app.listen(port, ()=> {
     console.log('Server running on the port',+ port);
